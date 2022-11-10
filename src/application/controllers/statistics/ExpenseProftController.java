ff7fe754984fcdc3e628e6e208156d04d5a9b9c7
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2022-11-10 12:43:29 +0100

    Design expenseProfit view
    Make design for the first time add lineChart and comboBox for select the type of sort.
    Add controller to it with init methode

---
+++ src/application/controllers/statistics/ExpenseProftController.java
@@ -0,0 +1,25 @@
+package application.controllers.statistics;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.layout.StackPane;
+
+public class ExpenseProftController implements Initializable {
+
+	@FXML
+    private LineChart<?, ?> expenseProfitChart;
+
+    
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
