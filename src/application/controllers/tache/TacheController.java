655917cf0905e4ec0e5b263038d6431ae5e85d72
yassine1024 <the.fire.of.hell.is.black@gmail.com>
2022-12-22 14:23:57 +0100

    Adding Light/Dark mode to NewTask view

--- src/application/controllers/tache/TacheController.java
+++ src/application/controllers/tache/TacheController.java
@@ -10,17 +10,23 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXTextField;
 
 import application.SQLiteJDBC;
+import application.controllers.login.ActivationController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class TacheController implements Initializable {
 
 	private int user_id;
 	private boolean success;
+	
+	@FXML
+    private AnchorPane stackPane;
+
 
 	public boolean isSuccess() {
 		return success;
@@ -92,7 +98,17 @@ public class TacheController implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 
+		this.setLightDarkMode();
 		
 	}
-
+public void setLightDarkMode() {
+		
+		if(!ActivationController.getMode()) {
+			this.stackPane.getStylesheets().clear();
+			
+			this.stackPane.getStylesheets().add("/css/light_mode/buttonSyle.css");
+			
+			this.stackPane.getStylesheets().add("/css/light_mode/styles.css");
+		}
+	}
 }
